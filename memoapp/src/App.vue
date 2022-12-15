8130fbe87d8c83dac26a135f337164f15e2ec9cc
ohdaira <ohdaira@beingcorp.co.jp>
2022-12-15 15:42:56 +0900

    Vue.js（create-vue）の課題【2回目の提出】
    create-vueを使って実装し直しました。

---
+++ memoapp/src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import MemoList from "./components/Memo-List.vue";
+</script>
+
+<template>
+  <div>
+    <MemoList msg="メモ帳!" class="MemoList" />
+
+    <RouterLink to="/addition" class="AddMemo">＋</RouterLink>
+  </div>
+  <div>
+    <RouterView />
+  </div>
+</template>
+
+<style scoped>
+.MemoList {
+  width: 100%;
+  display: inline-block;
+
+  text-align: left;
+}
+.AddMemo {
+  font-size: 30px;
+  text-align: left;
+}
+</style>
---
+++ memoapp/src/views/AdditionView.vue
@@ -0,0 +1,10 @@
+<script setup>
+import Addition from "../components/Memo-Add.vue";
+</script>
+
+<template>
+  <main>
+    <Addition />
+    <RouterLink to="/"><input type="submit" value="キャンセル" /></RouterLink>
+  </main>
+</template>
